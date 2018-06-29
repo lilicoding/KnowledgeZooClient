8fd54479b44277f31f5d9464ffed38070c2c617f
Li Li <lilicoding@gmail.com>
2018-06-29 09:57:38 +0200

    update launch script

--- runKnowledgeZooClient.sh
+++ runKnowledgeZooClient.sh
@@ -1,3 +1,14 @@
 #! /bin/sh
 
-java -Xmx40G -jar KnowledgeZooClient.jar -csv $1
+APK=$1
+
+SHA256=`shasum -a 256 $APK | awk '{print $1}' | tr '[a-z]' '[A-Z]'`
+
+echo "{" > $SHA256.json
+echo "$SHA256:" >> $SHA256.json
+./extract_metadata.py $APK >> $SHA256.json
+echo "}" >> $SHA256.json
+
+java -Xmx40G -jar KnowledgeZooClient.jar -csv $SHA256.json
+
+rm -rf $SHA256.json
